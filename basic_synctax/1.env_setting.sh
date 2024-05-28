ecf5023c6ffb533b25f40f397a922fef02badfce
kimseonguk197 <ksg39412@naver.com>
2024-05-28 17:31:39 +0900

    fix

--- basic_synctax/1.env_setting.sh
+++ basic_synctax/1.env_setting.sh
@@ -4,7 +4,11 @@
 # 방법2.로컬에서부터 이미 개발된 프로젝트가 존재시에 github 업로드
 # 해당 프로젝트 경로에 .git폴더 생성
 git init
-
 # 원격지 주소를 생성 및 추가
 # 원격지 주소 추가
 git remote add origin 레포주소
+# 원격지 주소 삭제
+git remote remove origin
+# 원격주소 변경
+git remote set-url origin 레포주소
+
